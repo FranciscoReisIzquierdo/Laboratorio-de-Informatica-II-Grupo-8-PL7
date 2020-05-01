53f8a936d89f8f3e7410140022ea4509552140ea
Francisco Izquierdo <franciaguia@hotmail.com>
2020-05-01 15:04:34 +0100

    Conclusão do projeto

--- Bot/main.c
+++ Bot/main.c
@@ -4,14 +4,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[ ]) {
-    if(argc<2){exit(0);}
-    ESTADO *e= inicializar_estado();
-    FILE * fin = fopen(argv[1], "r");
-    FILE * fout = fopen(argv[2], "w");
-    le_Tabuleiro(e,fin);
-    COORDENADA c= mcts(e, vizinhas(e));
+    if (argc < 2) { exit(0); }
+    ESTADO *e = inicializar_estado();
+    FILE *fin = fopen(argv[1], "r");
+    FILE *fout = fopen(argv[2], "w");
+    le_Tabuleiro(e, fin);
+    COORDENADA c = mcts(e, vizinhas(e));
     guarda_jogada(e, c);
     jogar(e, c);
-    guarda_tabuleiro(e, fout);
-    return 1;
+    if (jogoAcabou(e) == 1){
+        mostrar_tabuleiro(e);
+        exit(0);
+    }
+    else {
+        guarda_tabuleiro(e, fout);
+        return 1;
+    }
 }
