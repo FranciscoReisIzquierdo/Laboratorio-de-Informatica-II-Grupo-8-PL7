059dbabdaa415b0195da10d0eac6019ad6561cc3
Francisco Reis Izquierdo A93241 <62063067+FranciscoReisIzquierdo@users.noreply.github.com>
2021-02-01 09:30:30 +0000

    Documentação do Projeto

--- Doc/html/search/variables_8.js
+++ Doc/html/search/variables_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['valor_151',['valor',['../struct_l_i_s_t_a_l_i_g.html#a51ff9db7c96f254ed9d6c22ee51ab5f9',1,'LISTALIG']]]
+  ['valor_151',['valor',['../struct_l_i_s_t_a_l_i_g.html#a51ff9db7c96f254ed9d6c22ee51ab5f9',1,'LISTALIG']]] 
 ];
